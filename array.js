7aa425e1e5a46d59fc01e22dd6dca7a83b748a90
Masian Parkire <parkiremasian@gmail.com>
2023-03-14 12:30:12 +0300

    arraysjs

---
+++ array.js
@@ -0,0 +1,34 @@
+
+ let arr1 = [3,7,34,90,12];
+ let last = arr1[arr1.length -1];
+console.log(last);
+
+let arr2 = [true, "green", "where",12,56];
+let lastElement = arr2[arr2.length -1];
+console.log(lastElement);
+
+
+const b =["Cow", "Bird", "Snake", "Dog"];
+ let c = b.join()
+ console.log(c)
+
+
+var arr3 = [ -5, 9, 5, 3, 2, -3, 6, 8, 4, 1 ];
+let arr4 = arr3.sort((a,b)=>a-b);
+console.log(arr4);
+var arr = ["apple", "mango", "apple",
+            "orange", "mango", "mango"];
+            function remove(arr) {
+                return arr.filter((item, 
+                    index) => arr.indexOf(item) === index);
+            }
+              
+            console.log(remove(arr)); 
+  
+            function duplicate(arr){
+              return arr.filter((currentValue,currentIndex) =>arr.indexOf(currentValue) !==currentIndex);
+
+            }
+            console.log(duplicate(arr));
+
+  
